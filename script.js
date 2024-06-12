dfa9e15e561be43b773a33b3d5b4d736f1cbb505
kauanpereiramoraes <106698831+kauanpereiramoraes@users.noreply.github.com>
2024-06-12 14:44:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,37 +6,37 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado: "Assim que terminou a temporada, o corinthians planeja fazer uma reformulação no elenco , então ele está pensando em contratar dois meias, Quais contratar?",
-        alternativas: [
+        texto: "Assim que terminou a temporada, o corinthians planeja fazer uma reformulação no elenco , então ele está pensando em contratar dois meias, Quais contratar?",
+        afirmação: [
             "igor Coronado",
             "Rodrigo Garro"
         ]
     },
     {
-        enunciado: "Com a divida do Corinthians, ele planeja diminuir a divida com a entradado novo presidente, quem seria um bom nome para a presidencia do Corinthians?",
-        alternativas: [
+          texto: "Com a divida do Corinthians, ele planeja diminuir a divida com a entradado novo presidente, quem seria um bom nome para a presidencia do Corinthians?",
+          afirmação: [
             "augusto Melo",
             "André Negão"
         ]
     },
     {
-        enunciado: "com a saída do Cassio do Corinthians, o timão busca colocar um goleiro do elenco corintiano,qual seria um bom nome? "
+      texto: "com a saída do Cassio do Corinthians, o timão busca colocar um goleiro do elenco corintiano,qual seria um bom nome? "
     ",
-        alternativas: [
+          afirmação: [
             "Carlos Miguel",
             "Felipe Longo"
         ]
     },
     {
-        enunciado: "Com o fim do Paulistão, o corinthians busca melhorar seu elenco, e com isso busca um novo treinador, Qual seria o melhor nome?",
-        alternativas: [
+          texto: "Com o fim do Paulistão, o corinthians busca melhorar seu elenco, e com isso busca um novo treinador, Qual seria o melhor nome?",
+          afirmação: [
             "Antonio Oliveira",
             "Mano Menezes"
         ]
     },
     {
-        enunciado: "O corinthians buca um titulo que ainda é inedito na sua enorme pratileira de troféus, e com isso e ele ganhar esse titulo, ele vai se consagrar campeão de tudo, Qual titulo que falta?",
-        alternativas: [
+          texto: "O corinthians buca um titulo que ainda é inedito na sua enorme pratileira de troféus, e com isso e ele ganhar esse titulo, ele vai se consagrar campeão de tudo, Qual titulo que falta?",
+          afirmação: [
             "Sudamericana",
             "Libertadores"
         ]

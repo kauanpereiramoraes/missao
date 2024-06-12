18f3fdf475e55ae08cef10c5fa03afb46b02a2b7
kauanpereiramoraes <106698831+kauanpereiramoraes@users.noreply.github.com>
2024-06-12 15:29:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -55,10 +56,14 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
             botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click", function(){
-                atual++;
-                mostraPergunta();
-            })
-            caixaAlternativas.appendChild(botaoAlternativas);
-    }
+            botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+            caixaAlternativas.appendChild(botaoAlternativas);   
+           
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historia += afirmacoes + "";
+    atual++;
+    mostraPergunta();
 }
+
